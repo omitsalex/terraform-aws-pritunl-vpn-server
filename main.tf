f2012ed2c01f1d698529d71c84c86d4dff070daf
omitsalex <omitsalex@gmail.com>
2024-08-15 18:18:10 +0100

    update template_file to cloud_init

--- main.tf
+++ main.tf
@@ -6,53 +6,74 @@ locals {
   backup_bucket_name = var.s3_bucket_name != "" ? var.s3_bucket_name : format("%s-%s", var.resource_name_prefix, "backup")
 }
 
-data "template_file" "user_data" {
-  template = templatefile("${path.module}/templates/user_data.sh.tpl",
-    {
-      aws_region          = data.aws_region.current.name
-      s3_backup_bucket    = local.backup_bucket_name
-      healthchecks_io_key = "/pritunl/${var.resource_name_prefix}/healthchecks-io-key"
-    }
-  )
-}
+# Generate the cloud-init user data
+data "cloudinit_config" "user_data" {
+  gzip          = false
+  base64_encode = false
 
-data "template_file" "kms_policy" {
-  template = templatefile("${path.module}/templates/key_policy.json.tpl", {
-    resource_name_prefix = var.resource_name_prefix
-    account_id           = data.aws_caller_identity.current.account_id
-    key_admin_arn        = aws_iam_role.role.arn
-    }
-  )
-}
-
-data "template_file" "iam_instance_role_policy" {
-  template = templatefile("${path.module}/templates/iam_instance_role_policy.json.tpl", {
-    s3_backup_bucket     = local.backup_bucket_name
-    resource_name_prefix = var.resource_name_prefix
-    aws_region           = data.aws_region.current.name
-    account_id           = data.aws_caller_identity.current.account_id
-    ssm_key_prefix       = "/pritunl/${var.resource_name_prefix}/*"
-    }
-  )
-}
-
-resource "null_resource" "waiter" {
-  depends_on = [aws_iam_instance_profile.ec2_profile]
-
-  provisioner "local-exec" {
-    command = "sleep 15"
+  part {
+    content_type = "text/x-shellscript"
+    content      = templatefile("${path.module}/templates/user_data.sh.tpl",
+      {
+        aws_region          = data.aws_region.current.name
+        s3_backup_bucket    = local.backup_bucket_name
+        healthchecks_io_key = "/pritunl/${var.resource_name_prefix}/healthchecks-io-key"
+      }
+    )
   }
 }
 
+data "aws_iam_policy_document" "kms_policy" {
+  statement {
+    actions   = ["kms:*"]
+    resources = ["*"]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+  }
+
+  statement {
+    actions   = ["kms:Create*", "kms:Describe*", "kms:Enable*", "kms:List*", "kms:Put*", "kms:Update*", "kms:Revoke*", "kms:Disable*", "kms:Get*", "kms:Delete*", "kms:ScheduleKeyDeletion", "kms:CancelKeyDeletion"]
+    resources = ["*"]
+    principals {
+      type        = "AWS"
+      identifiers = ["${aws_iam_role.role.arn}"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "iam_instance_role_policy" {
+  statement {
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation",
+      "s3:AbortMultipartUpload",
+      "s3:PutObject*",
+      "s3:Get*",
+      "s3:List*",
+      "s3:DeleteObject",
+      "ssm:GetParameters"
+    ]
+    resources = [
+      "arn:aws:s3:::${local.backup_bucket_name}",
+      "arn:aws:s3:::${local.backup_bucket_name}/*",
+      "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/pritunl/${var.resource_name_prefix}/*"
+    ]
+  }
+
+  statement {
+    actions   = ["ec2messages:*", "cloudwatch:PutMetricData", "ec2:DescribeInstanceStatus", "ds:*", "logs:*"]
+    resources = ["*"]
+  }
+}
+
+# KMS Key
 resource "aws_kms_key" "parameter_store" {
-  depends_on = [null_resource.waiter]
-
-  description = "Parameter store and backup key for ${var.resource_name_prefix}"
-
-  policy                  = data.template_file.kms_policy.rendered
-  deletion_window_in_days = 30
-  is_enabled              = true
-  enable_key_rotation     = true
+  description              = "Parameter store and backup key for ${var.resource_name_prefix}"
+  policy                   = data.aws_iam_policy_document.kms_policy.json
+  deletion_window_in_days  = 30
+  enable_key_rotation      = true
 
   tags = merge(
     tomap({"Name" = format("%s-%s", var.resource_name_prefix, "parameter-store")}),
@@ -61,59 +82,11 @@ resource "aws_kms_key" "parameter_store" {
 }
 
 resource "aws_kms_alias" "parameter_store" {
-  depends_on = [aws_kms_key.parameter_store]
-
   name          = "alias/${var.resource_name_prefix}-parameter-store"
   target_key_id = aws_kms_key.parameter_store.key_id
 }
 
-resource "aws_ssm_parameter" "healthchecks_io_key" {
-  name      = "/pritunl/${var.resource_name_prefix}/healthchecks-io-key"
-  type      = "SecureString"
-  value     = var.healthchecks_io_key
-  key_id    = aws_kms_key.parameter_store.arn
-  overwrite = true
-
-  tags = merge(
-    tomap({"Name" = format("%s/%s/%s", "pritunl", var.resource_name_prefix, "healthchecks-io-key")}),
-    var.tags,
-  )
-}
-
-resource "aws_s3_bucket" "backup" {
-  depends_on = [aws_kms_key.parameter_store]
-
-  bucket = local.backup_bucket_name
-
-  acl = "private"
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.parameter_store.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-
-  lifecycle_rule {
-    prefix  = "backups"
-    enabled = true
-
-    expiration {
-      days = 30
-    }
-
-    abort_incomplete_multipart_upload_days = 7
-  }
-
-  tags = merge(
-    tomap({"Name" = local.backup_bucket_name}),
-    var.tags,
-  )
-}
-
-# ec2 iam role
+# EC2 IAM Role and Policies
 resource "aws_iam_role" "role" {
   name = var.resource_name_prefix
 
@@ -135,26 +108,22 @@ EOF
 }
 
 resource "aws_iam_role_policy" "policy" {
-  depends_on = [aws_iam_role.role]
-
   name   = "${var.resource_name_prefix}-instance-policy"
   role   = aws_iam_role.role.id
-  policy = data.template_file.iam_instance_role_policy.rendered
+  policy = data.aws_iam_policy_document.iam_instance_role_policy.json
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
-  depends_on = [aws_iam_role.role, aws_iam_role_policy.policy]
-
   name = "${var.resource_name_prefix}-instance"
   role = aws_iam_role.role.name
 }
 
+# Security Groups
 resource "aws_security_group" "pritunl" {
   name        = "${var.resource_name_prefix}-vpn"
   description = "${var.resource_name_prefix}-vpn"
   vpc_id      = var.vpc_id
 
-  # SSH access
   ingress {
     from_port   = 22
     to_port     = 22
@@ -162,16 +131,13 @@ resource "aws_security_group" "pritunl" {
     cidr_blocks = var.internal_cidrs
   }
 
-  # HTTP access for Let's Encrypt validation
   ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
-
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = var.whitelist_http
   }
 
-  # HTTPS access
   ingress {
     from_port   = 443
     to_port     = 443
@@ -179,7 +145,6 @@ resource "aws_security_group" "pritunl" {
     cidr_blocks = var.internal_cidrs
   }
 
-  # VPN WAN access
   ingress {
     from_port   = 10000
     to_port     = 19999
@@ -187,7 +152,6 @@ resource "aws_security_group" "pritunl" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # ICMP
   ingress {
     from_port   = -1
     to_port     = -1
@@ -195,7 +159,6 @@ resource "aws_security_group" "pritunl" {
     cidr_blocks = var.internal_cidrs
   }
 
-  # outbound internet access
   egress {
     from_port   = 0
     to_port     = 0
@@ -260,7 +223,7 @@ resource "aws_instance" "pritunl" {
   instance_type = var.instance_type
   key_name      = var.aws_key_name
   ebs_optimized = var.ebs_optimized
-  user_data     = data.template_file.user_data.rendered
+  user_data     = data.cloudinit_config.user_data.rendered
 
   vpc_security_group_ids = [
     aws_security_group.pritunl.id,
